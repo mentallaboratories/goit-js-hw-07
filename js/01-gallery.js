79ca4517309b33ea1ab62fe6d0c2667e5883426e
mentallaboratories <mentallaboratories@gmail.com>
2023-01-21 02:52:01 +0200

    hw lightBox to finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,79 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+//1STEP Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const imageGalleryContainer = document.querySelector('.gallery');
+const markup = createImageGalleryMarkup(galleryItems);
+
+imageGalleryContainer.innerHTML = markup;
+
+//console.log(createImageGalleryMarkup(galleryItems));
+
+function createImageGalleryMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) =>{
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `
+    }).join('')
+};
+
+
+//2STEP Реалізація делегування на div.gallery і отримання url великого зображення.
+
+imageGalleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(e){ 
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')){
+        return;
+    }
+
+    
+    const image = e.target;
+   // console.log(image.parentNode.href);
+    const imageOriginalSrc = image.parentNode.href;
+
+    
+
+    window.addEventListener('keydown', onEscKeyPress);
+
+    const instance = basicLightbox.create(
+        
+        `
+ 		<img width="1280" height="855" src="${imageOriginalSrc}">
+        	`)
+            instance.show()
+       
+    function onEscKeyPress(e){
+        if(!instance.visible){
+            return
+        }
+        instance.close()
+        console.log('close')
+        window.removeEventListener('keydown', onEscKeyPress)
+    }
+        
+}
+
+//ESC 
+
+
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+//1STEP Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const imageGalleryContainer = document.querySelector('.gallery');
+const markup = createImageGalleryMarkup(galleryItems);
+
+imageGalleryContainer.innerHTML = markup;
+
+//console.log(createImageGalleryMarkup(galleryItems));
+
+function createImageGalleryMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) =>{
+        return `
+         <div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+            </a>
+        </div>
+         `
+    }).join('')
+};
+
+imageGalleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(e){ 
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')){
+        return;
+    }
+    
+
+
+    let gallery = new SimpleLightbox('.gallery a');
+    gallery.on('show.simplelightbox', function () {
+        
+    });
+}
