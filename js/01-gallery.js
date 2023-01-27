098d5a90aa0e6b5e2b7e8b8ba7d281e23677b682
mentallaboratories <mentallaboratories@gmail.com>
2023-01-27 03:04:05 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,7 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
